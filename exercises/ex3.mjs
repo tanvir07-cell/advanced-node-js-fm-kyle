d42ee5de4beae81f60c8c13a9828fb1d1e75cb8f
tanvir rifat <tr9836859@gmail.com>
2024-01-20 22:49:44 +0700

    Chore:for the abort

--- exercises/ex3.mjs
+++ exercises/ex3.mjs
@@ -1,144 +1,148 @@
 #!/usr/bin/env node
-import yargs from 'yargs'
-import { hideBin } from 'yargs/helpers'
 
-
-import c from 'ansi-colors';
-
-// const c = require('ansi-colors');
-// const path = require("path");
-import fs from "node:fs"
-// const fs = require('fs')
-
-import getStdin from 'get-stdin';
-import { Transform } from 'node:stream';
-
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+import fs from 'fs';
+import through2 from 'through2';
 import zlib from 'node:zlib';
+import { AbortController } from 'abort-controller';
 
-
-
-let BASE_PATH = new URL("./out.txt",import.meta.url).pathname;
+let BASE_PATH = new URL('./out.txt', import.meta.url).pathname;
 
 yargs(hideBin(process.argv))
-  .command('outStream <file>', 'fetch the contents of the URL', yargs => {
-    return yargs.positional('file', {
-      describe: 'The search term to filter notes by, will be applied to note.content',
-      type: 'string'
-    })
-  }, (argv) => {
-    const filePath = new URL(argv.file,import.meta.url).pathname;
+  .command('print', 'read the content of the file', () => {},  (argv) => {
+    console.log(argv);
 
-    const stream = fs.createReadStream(filePath)   
-    processFile(stream)
-    console.log("completed!")
+    if (argv.file) {
+      try {
+        const pathToFile = new URL(argv.file, import.meta.url).pathname;
+        const readStream = fs.createReadStream(pathToFile);
 
+        // Wrap the processFile operation with a timeout and abort controller
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => {
+          console.log('Timeout reached. Stopping the stream.');
+          controller.abort();
+        }, argv.timeout || 5000); // Set a default timeout of 5 seconds
+
+        // Attach the timeoutId and controller to argv for later cleanup
+        argv.timeoutId = timeoutId;
+        argv.controller = controller;
+
+         processFile(readStream, argv);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    if (argv.in || argv._.includes('-')) {
+      // Wrap the processFile operation with a timeout and abort controller
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => {
+        console.log('Timeout reached. Stopping the stream.');
+        controller.abort();
+      }, argv.timeout || 5000); // Set a default timeout of 5 seconds
+
+      // Attach the timeoutId and controller to argv for later cleanup
+      argv.timeoutId = timeoutId;
+      argv.controller = controller;
+
+       processFile(process.stdin, argv);
+    }
   })
-    .option('out', {
+  .option('file', {
+    alias: 'f',
+    type: 'string',
+    description: 'file to read',
+  })
+  .option('in', {
+    alias: 'i',
+    type: 'boolean',
+    description: 'read from stdin',
+  })
+  .option('out', {
     alias: 'o',
-    type: 'string',
-    description: 'tags to add to the note'
+    type: 'boolean',
+    description: 'write to stdout',
   })
-   .option('compress', {
+  .option('compress', {
     alias: 'c',
-    type: 'string',
-    description: 'tags to add to the note'
+    type: 'boolean',
+    description: 'compress output',
   })
   .option('uncompress', {
     alias: 'u',
-    type: 'string',
-    description: 'tags to add to the note'
+    type: 'boolean',
+    description: 'uncompress output',
   })
-
-  .option('in', {
-    alias: 'i',
-    type: 'string',
-    description: 'tags to add to the note'
+  .option('timeout', {
+    alias: 't',
+    type: 'number',
+    description: 'timeout in milliseconds',
   })
-  // .command('writeStream <file>', 'fetch the contents of the URL', () => {}, (argv) => {
-  //   const filePath = new URL(argv.file,import.meta.url).pathname;
+  .demandCommand(1, 'You need at least one command before moving on')
+  .parse();
 
-  //   const stream = fs.createWriteStream(filePath)   
-  //   processFile(stream)
-  // })
-
-
-  // .command('in', 'fetch the contents of the URL', () => {}, (argv) => {
-  //   console.log(argv)
-  //      getStdin().then(processFile).catch(err=>console.error(err))
-
-  // })
-  // .demandCommand(1,c.red.bold.underline('You need at least one command before moving on'))
-  // .strictCommands(true)
- 
-  .help()
-  .parse()
-  
-
-if(yargs(hideBin(process.argv)).argv.in){
-  processFile(process.stdin)
-}
-
-
-
-function processFile(inStream){
-  // inStream is the readble stream
+function processFile(inStream, argv) {
   let outStream = inStream;
 
-  if(yargs(hideBin(process.argv)).argv.uncompress){
-    // here gunzipStream is the readable transform stream
-    var gunzipStream = zlib.createGunzip();
-    outStream = outStream.pipe(gunzipStream)
-
+  if (yargs(hideBin(process.argv)).argv.uncompress) {
+    const gunzipStream = zlib.createGunzip();
+    outStream = outStream.pipe(gunzipStream);
+    BASE_PATH = BASE_PATH.slice(0);
   }
 
-  // here upperStream is the writable transform stream
-  var upperStream = new Transform({
-    transform(chunk,encoding,cb){
-      this.push(chunk.toString().toUpperCase())
-      cb()
-    }
+  outStream = outStream.pipe(
+    through2(function (buf, enc, next) {
+      if (argv.controller.signal.aborted) {
+        // Stream is aborted, stop processing
+        console.log('Stream aborted. Stopping the stream.');
+        return;
+      }
+      next(null, buf.toString().toUpperCase());
+    })
+  );
 
-  })
-
-  outStream = outStream.pipe(upperStream)
-
-  //if there is --compress flag:
-  if(yargs(hideBin(process.argv)).argv.compress){
-    // here gzipStream is the writable transform stream
-    var gzipStream = zlib.createGzip();
-    outStream = outStream.pipe(gzipStream)
-    // change the file extension from .txt to .txt.gz:
-    BASE_PATH = `${BASE_PATH}.gz`
+  if (yargs(hideBin(process.argv)).argv.compress) {
+    const gzipStream = zlib.createGzip();
+    outStream = outStream.pipe(gzipStream);
+    BASE_PATH = `${BASE_PATH}.gz`;
   }
 
+  let targetStream;
 
-
-  var targetStream;
-
-  // if there is --out flag:
-  if(yargs(hideBin(process.argv)).argv.out){
-       targetStream = process.stdout;
-
+  if (yargs(hideBin(process.argv)).argv.out) {
+    targetStream = process.stdout;
+  } else {
+    targetStream = fs.createWriteStream(BASE_PATH);
   }
 
+  outStream.pipe(targetStream);
 
+  return new Promise((resolve, reject) => {
+    outStream.on('end', () => {
+      if (targetStream !== process.stdout) {
+        console.log(`File written to ${BASE_PATH}`);
+      }
+      if (targetStream === process.stdout) {
+        console.log(`File written to stdout`);
+      }
+      resolve();
+    });
 
-  else{
-    targetStream = fs.createWriteStream(BASE_PATH)
-  }
+    // Handle 'error' event
+    outStream.on('error', (err) => {
+      if (argv.controller.signal.aborted) {
+        // Stream is aborted, don't log error
+        resolve();
+      } else {
+        reject(err);
+      }
+    });
 
-
-
-  outStream.pipe(targetStream) // looks like : readableStrea.pipe(writableStream)
-
-
+    // Cleanup timeout and controller on stream end
+    outStream.on('end', () => {
+      clearTimeout(argv.timeoutId);
+    });
+  });
 }
-
-
-  
-console.log(yargs(hideBin(process.argv)).argv)
-
-
-
-
-
